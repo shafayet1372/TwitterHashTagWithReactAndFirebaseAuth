66283fbde249a4798e082165bf449829f3803af6
shafayet <shafayethossain777@gmail.com>
2022-04-26 05:21:42 +0600

    file changed

--- src/components/createTwitt/index.jsx
+++ src/components/createTwitt/index.jsx
@@ -3,6 +3,7 @@ import { collection, addDoc } from "firebase/firestore";
 import React, { useState, useContext, useRef, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { errorHandlerTwitts } from "../../js";
+import { HighlightWithinTextarea } from "react-highlight-within-textarea";
 import style from "../../style.module.css";
 import context from "../../context";
 import TwittSuggestionsView from "../TwittSuggestionView";
@@ -11,15 +12,13 @@ export default function Index({ handleClose }) {
   let [error, setError] = useState("");
   let [twiitSuggestions, setTwiitSuggestions] = useState([]);
   let [caretPosition, setCareposition] = useState(null);
-  let [caretEndPosition, setCaretEndposition] = useState(null);
-  let refs = useRef();
-  useEffect(() => {
-    if (caretEndPosition) {
-      refs.current.focus();
+  // let [caretEndPosition, setCaretEndposition] = useState(null);
 
-      refs.current.setSelectionRange(caretEndPosition, caretEndPosition + 1);
+  useEffect(() => {
+    if (post) {
+      twittSuggestionsView(post, caretPosition);
     }
-  }, [caretEndPosition]);
+  }, [post]);
   let allTwits = useContext(context);
 
   let collections = collection(db, "twitterposts");
@@ -44,22 +43,19 @@ export default function Index({ handleClose }) {
     }
   };
 
-  let changeHandler = (e) => {
-    setPost(e.target.value);
-  };
   let getTwittSuggestion = (data, value) => {
     let searchTwit = value.match(/#[\w]+$/gi);
     let getallTwitt = data.filter((x) =>
       new RegExp(`${searchTwit}`, "i").test(x[0])
     );
-    //
+
     return getallTwitt;
   };
-  let twittSuggestionsView = (e) => {
-    let caretPosition = e.target.selectionStart;
-    let value = e.target.value.slice(0, caretPosition);
+  let twittSuggestionsView = (values, e) => {
+    let caretPosition = e;
+    let value = values.slice(0, caretPosition);
     if (/#[\w]{2}$|#[\w]{2,}$/i.test(value)) {
-      let getTwitts = getTwittSuggestion(allTwits, e.target.value);
+      let getTwitts = getTwittSuggestion(allTwits, value);
       setTwiitSuggestions((p) => [...getTwitts]);
       setCareposition(caretPosition);
     } else {
@@ -79,9 +75,13 @@ export default function Index({ handleClose }) {
         return s;
       })
     );
-    // setCareposition(null);
-    setCaretEndposition(index + value.length);
+    setCareposition(null);
+    // setCaretEndposition(index + value.length);
     setTwiitSuggestions([]);
+  };
+  let onChange = (value, selection) => {
+    setCareposition(selection.anchor);
+    setPost(value);
   };
   return (
     <Form>
@@ -90,16 +90,28 @@ export default function Index({ handleClose }) {
           className={`${style.textarea} mb-3`}
           controlId="exampleForm.ControlTextarea1  "
         >
-          <Form.Control
+          <HighlightWithinTextarea
+            value={post}
+            placeholder=""
+            highlight={{
+              highlight: /#[\w]+/gi,
+              className: style.highlights,
+            }}
+            // onKeyUp={twittSuggestionsView}
+            // selection={selection}
+            onChange={onChange}
+          />
+          {/* <Form.Control
             as="textarea"
             rows={6}
             value={post}
             onChange={changeHandler}
             onKeyUp={twittSuggestionsView}
             ref={refs}
-          />
+          /> */}
           {error && <p className={style.twitt_error}>{error}</p>}
         </Form.Group>
+
         <TwittSuggestionsView
           twiitSuggestions={twiitSuggestions}
           SelectTwittHandler={SelectTwittHandler}
